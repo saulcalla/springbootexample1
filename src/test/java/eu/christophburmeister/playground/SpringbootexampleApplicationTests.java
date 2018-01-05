1223ea73a52dec19b10fde7ae58e602d9df67364
saulcalla <saulcalla@gmail.com>
2018-01-05 04:24:31 -0300

    inicio

---
+++ src/test/java/eu/christophburmeister/playground/SpringbootexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.christophburmeister.playground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={SpringbootexampleApplicationTests.class})
+public class SpringbootexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
